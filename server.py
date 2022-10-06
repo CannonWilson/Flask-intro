08e90c29057fe4daaf467081ebbe6d58c89a86a6
CannonWilson <CannonGWilson@gmail.com>
2022-10-06 17:24:40 -0500

    Added second route

--- server.py
+++ server.py
@@ -1,7 +1,12 @@
 from flask import Flask
+from markupsafe import escape
 
 app = Flask(__name__) # create instance of Flask class
 
-@app.route("/") # create route decorator
+@app.route("/") # create route decorator to bind function to route
 def hello_world(): # function triggered at route
     return "<p>Hello, World!</p>"
+
+@app.route("/<name>")
+def print_name(name):
+    return f"Hello, {escape(name)}"
